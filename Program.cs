f6ac3ab28f68a93250435d31e86de6d163bc428c
Jordi Sanchez <jordi@friendlyrentals.com>
2016-12-09 11:42:16 +0100

    Project setup

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fr_stringcalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Friendlyrentals string calculator kata";
+            Console.WriteLine("Follow instructions on 'readme.md'");
+            Console.ReadLine();
+        }
+    }
+}
