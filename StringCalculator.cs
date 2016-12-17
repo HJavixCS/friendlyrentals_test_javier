3b12d8030846ae87c7f8cb3ed52e67e4e2c6d50a
Javier Castillo <javier.castillosuazo@gmail.com>
2016-12-17 22:44:19 +0100

    Create StringCalculator class. 1st iteration Add method receiving any arguments. Add NUnit nuget package to test.

---
+++ StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fr_stringcalculator
+{
+  public class StringCalculator
+  {
+    public int Add()
+    {
+      return 0;
+    }
+  }
+}
---
+++ fr-stringcalculator.Test/StringCalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using fr_stringcalculator;
+using NUnit.Framework;
+
+
+namespace fr_stringcalculator.Test
+{
+  [TestFixture]
+  public class StringCalculatorTest
+  {
+    [Test]
+    public void Given_TheStringCalculator_When_EnterNothing_Then_ReturnZero()
+    {
+      var calculator = new StringCalculator();
+      var expected = 0;
+      var actual = calculator.Add();
+      Assert.AreEqual(expected, actual);
+    }
+  }
+}
